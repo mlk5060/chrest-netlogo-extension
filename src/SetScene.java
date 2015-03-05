6f5968d23821a1cdee6de916d3d2637d8823da1c
martyn <martynlloydkelly@gmail.com>
2015-03-05 17:21:57 +0000

    Fixed incorrect 'sight-radius' variable specification

--- src/SetScene.java
+++ src/SetScene.java
@@ -32,7 +32,7 @@ public class SetScene extends DefaultCommand {
     
     try {
       //Get the calling agent's sight radius value.
-      int sightRadius = (int)callingAgent.getBreedVariable("sight-radius");
+      int sightRadius = (int)callingAgent.getBreedVariable("sight-radius".toUpperCase());
       
       //The scene will consist of sightRadius squares north/east/south/west of
       //the agent's current location.  So multiply the value of sightRadius by 2
