313d50f8e71f1e69cd6d9037adf5853d94a87cac
Martyn Lloyd-Kelly <martynlloydkelly@gmail.com>
2016-07-20 11:26:59 +0100

    Can create semantic links/templates fix

---
+++ src/org/nlogo/extensions/chrest/architecture/chrest/SetCreateTemplates.java
@@ -0,0 +1,38 @@
+package org.nlogo.extensions.chrest.architecture.chrest;
+
+import org.nlogo.api.Argument;
+import org.nlogo.api.Context;
+import org.nlogo.api.DefaultCommand;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.LogoException;
+import org.nlogo.api.Syntax;
+import org.nlogo.extensions.chrest.ChrestExtension;
+
+/**
+ *
+ * @author Martyn Lloyd-Kelly <martynlk@liverpool.ac.uk>
+ */
+public class SetCreateTemplates extends DefaultCommand {
+  
+  @Override
+  public Syntax getSyntax(){
+    return Syntax.commandSyntax(new int[]{Syntax.BooleanType()});
+  }
+
+  /**
+   * Invokes {@link jchrest.architecture.Chrest#setCreateTemplates(boolean)} in
+   * context of the calling turtle's {@link jchrest.architecture.Chrest} model.
+   * 
+   * @param args See parameter descriptions for {@link 
+   * jchrest.architecture.Chrest#setCreateTemplates(boolean)}.
+   * @param context
+   * 
+   * @throws ExtensionException
+   * @throws LogoException 
+   */
+  @Override
+  public void perform(Argument[] args, Context context) throws ExtensionException, LogoException {
+    ChrestExtension.getTurtlesChrestInstance(context).setCreateTemplates(args[0].getBooleanValue());
+  }
+  
+}
